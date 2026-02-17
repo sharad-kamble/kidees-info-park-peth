df73d5bf77e1b4b520b5065592b0ffd2a18063be
sharad kamble <sharadkamble131@gmail.com>
2026-02-18 00:22:07 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let groupImages = [];
+let currentIndex = 0;
+
+function openGroup(images) {
+    groupImages = images;
+    currentIndex = 0;
+    document.getElementById("lightbox").classList.add("show");
+    document.getElementById("lightboxImg").src = groupImages[currentIndex];
+}
+
+function changeSlide(step) {
+    currentIndex = (currentIndex + step + groupImages.length) % groupImages.length;
+    document.getElementById("lightboxImg").src = groupImages[currentIndex];
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").classList.remove("show");
+}
