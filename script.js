3fa06a1b13b22e21a0e3c83330a8e3037b4416ae
sharad kamble <sharadkamble131@gmail.com>
2026-02-27 23:16:15 +0530

    ui updated

--- script.js
+++ script.js
@@ -65,3 +65,57 @@ document.querySelectorAll('.sidebar-link').forEach(link => {
 });
 
 
+/* Lightbox Preview */
+function showImage(element) {
+    var img = element.querySelector("img").src;
+    document.getElementById("modalImage").src = img;
+}
+
+/* Filter System */
+const filterButtons = document.querySelectorAll('.filter-btn');
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+filterButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+
+        filterButtons.forEach(b => b.classList.remove('active'));
+        btn.classList.add('active');
+
+        const filter = btn.getAttribute('data-filter');
+
+        galleryItems.forEach(item => {
+            if (filter === 'all' || item.classList.contains(filter)) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+
+    });
+});
+
+
+
+// fast load img gallary page 
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const modal = new bootstrap.Modal(document.getElementById('galleryModal'));
+    const modalImg = document.getElementById("modalImage");
+
+    // Open modal
+    document.querySelectorAll(".home-gallery-card").forEach(card => {
+        card.addEventListener("click", function () {
+            const imgSrc = this.getAttribute("data-img");
+            modalImg.src = imgSrc;
+            modal.show();
+        });
+    });
+
+    // Clear image when modal closes (important for speed)
+    document.getElementById('galleryModal').addEventListener('hidden.bs.modal', function () {
+        modalImg.src = "";
+    });
+
+});
+
